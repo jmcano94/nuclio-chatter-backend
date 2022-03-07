fdeb3e16807488bfbc5b66af202a5bb116481a58
Jose Manuel Cano <jmcano94@gmail.com>
2022-03-07 19:42:47 +0100

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 const server = app.listen(process.env.PORT, () => {
 	console.log('server is running on port', process.env.PORT);
 });
